9bd641f41bdb71de123dcb51c21428fccca4a8d7
James Portman <James.Portman3496@gmail.com>
2021-02-07 13:57:22 +0000

    second

---
+++ second.py
@@ -0,0 +1,14 @@
+#Program using variables
+print("Just a bit bigger")
+print("Here", end=" ")
+print('it is...')
+
+print("""
+
+        GAME OVER
+
+                    OOOOo
+
+""")
+
+input("\n\nPress the enter key to exit")
